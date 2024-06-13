2301ab65560853b111a5d342b439333951d0d33e
JhefersonCh <divegas43@gmail.com>
2024-06-13 17:14:40 -0500

    change index name

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require("sequelize");
-const { sequelize } = require("../config");
+const { sequelize } = require("../config/sequelizeConfig");
 
 const User = sequelize.define(
 	"users",
