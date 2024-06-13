7f150c9b71b8ca093b0a139c4a714d3987de796f
JhefersonCh <divegas43@gmail.com>
2024-06-13 18:23:02 -0500

    fix

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ const { sequelize } = require("./src/config");
 const userRouter = require("./src/Routes/user.routes");
 const authRouter = require("./src/Routes/auth.routes");
 const noteRouter = require("./src/Routes/note.routes");
-const environment = require("./src/config/environment");
 
 const app = express();
 const port = process.env.PORT || 3000;
-const host = environment.config.HOST || '0.0.0.0';
 
 app.use(express.json());
 app.use(cors());
@@ -32,7 +30,7 @@ sequelize
   })
   .then(() => {
     console.log("Sync models");
-    app.listen(port, host, () => {
+    app.listen(port, () => {
       console.log(`Server listen on http://localhost:${port}`);
     });
   })
