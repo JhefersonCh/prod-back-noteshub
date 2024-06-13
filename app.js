37b38b9a01fc7c7982c57ca4ca79f222f00bef13
JhefersonCh <divegas43@gmail.com>
2024-06-13 17:06:16 -0500

    fixes for deploy

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-	res.send("Hello world!");
+  res.send("Hello world!");
 });
 
 app.use("/", userRouter);
@@ -23,17 +23,18 @@ app.use("/", noteRouter);
 app.use("/public", express.static("public"));
 
 sequelize
-	.authenticate()
-	.then(() => {
-		console.log("Connection success");
-		return sequelize.sync({ alter: true });
-	})
-	.then(() => {
-		console.log("Sync models");
-		app.listen(port, () => {
-			console.log(`Server listen on http://localhost:${port}`);
-		});
-	})
-	.catch((error) => {
-		console.error("Connection fail", error);
-	});
+  .authenticate()
+  .then(() => {
+    console.log("Connection success");
+    return sequelize.sync({ alter: true });
+  })
+  .then(() => {
+    console.log("Sync models");
+    app.listen(port, () => {
+      console.log(`Server listen on http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Connection fail", error);
+    process.exit(1); // Forzar la terminación del proceso en caso de error de conexión
+  });
