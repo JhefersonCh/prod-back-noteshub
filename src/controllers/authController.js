8d76afd8d4e411a41a39f0107ea2aec6618a82de
Jheff <divegas43@gmail.com>
2024-12-14 21:27:33 -0500

    Adjusts tokens

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,70 +1,73 @@
-const { User, RefreshToken } = require("../models/index");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const { v4: uuidv4 } = require("uuid");
+const { User, RefreshToken } = require('../models/index');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { v4: uuidv4 } = require('uuid');
 
 exports.login = async (req, res) => {
-  
   try {
     const { email, password } = req.body;
 
-    console.log( req.body );
-
-    if(!email || !password){
+    if (!email || !password) {
       return res.status(400).json({
-        error: 'Email and password are required.'
-      })
+        error: 'Email and password are required.',
+      });
     }
 
     const userExist = await User.findOne({ where: { email } });
 
-    if(!userExist){
+    if (!userExist) {
       return res.status(400).json({
-				error: "This email does not exist.",
-			});
+        error: 'This email does not exist.',
+      });
     }
-    const passwordIsValid = await bcrypt.compare(password, userExist.password)
+    const passwordIsValid = await bcrypt.compare(password, userExist.password);
 
-    if(!passwordIsValid){
+    if (!passwordIsValid) {
       return res.status(400).json({
-				error: "The password is invalid.",
-			});
+        error: 'The password is invalid.',
+      });
     }
 
     const token = jwt.sign(
       {
-       id: userExist.id, email: userExist.email 
+        id: userExist.id,
+        email: userExist.email,
       },
       process.env.JWT_SECRET,
       {
-        algorithm: "HS256",
+        algorithm: 'HS256',
         allowInsecureKeySizes: true,
-        expiresIn: process.env.JWT_EXPIRATION,
-      }
-    )
+        expiresIn: parseInt(process.env.JWT_SECRET_EXPIRATION, 10),
+      },
+    );
 
     const _token = uuidv4();
     let expireAt = new Date();
 
-	  expireAt.setTime(expireAt.getTime() + process.env.REFRESH_EXPIRATION * 1000);
+    expireAt.setTime(
+      expireAt.getTime() +
+        parseInt(process.env.JWT_REFRESH_EXPIRATION, 10) * 1000,
+    );
 
     const refreshToken = jwt.sign(
       {
-       id: userExist.id, email: userExist.email, uniqueId: _token
+        id: userExist.id,
+        email: userExist.email,
+        uniqueId: _token,
       },
-      process.env.REFRESH_SECRET,
+      process.env.REFRESH_TOKEN_SECRET,
       {
-        algorithm: "HS256",
+        algorithm: 'HS256',
         allowInsecureKeySizes: true,
-        expiresIn: parseInt(process.env.REFRESH_EXPIRATION),
-      }
-    )
+        expiresIn: parseInt(process.env.JWT_REFRESH_EXPIRATION),
+      },
+    );
 
     await RefreshToken.create({
       token: refreshToken,
       userId: userExist.id,
-      expiration: expireAt.getTime()
-    })
+      expiration: expireAt.getTime(),
+    });
 
     return res.status(200).json({
       message: 'Sign in successfully.',
@@ -73,17 +76,16 @@ exports.login = async (req, res) => {
           id: userExist.id,
           email: userExist.email,
           accessToken: token,
-          refreshToken: refreshToken
-        }
-      }
-    })
-    
+          refreshToken: refreshToken,
+        },
+      },
+    });
   } catch (error) {
     return res.status(500).json({
-      error: 'Internal server error.'
-    })
+      error: 'Internal server error.',
+    });
   }
-}
+};
 
 exports.refreshToken = async (req, res) => {
   try {
@@ -91,40 +93,44 @@ exports.refreshToken = async (req, res) => {
 
     if (!refreshToken) {
       return res.status(404).json({
-        message: "Refresh token is required",
+        message: 'Refresh token is required',
       });
     }
 
     const tokenExist = await RefreshToken.findOne({
-			where: { token: refreshToken },
-		});
+      where: { token: refreshToken },
+    });
 
-    if(!tokenExist){
+    if (!tokenExist) {
       return res.status(404).json({
-				message: "Refresh Token does not exist.",
-			});
+        message: 'Refresh Token does not exist.',
+      });
     }
 
-    if(RefreshToken.verifyExpiration(tokenExist)){
+    if (RefreshToken.verifyExpiration(tokenExist)) {
       await RefreshToken.destroy({ where: { id: tokenExist.id } });
       return res.status(403).json({
-				message: "Refresh token was expired. Please make a new signin request.",
-			});
+        message: 'Refresh token was expired. Please make a new signin request.',
+      });
     }
 
     const user = await tokenExist.getUser();
 
-    if(!user){
+    if (!user) {
       return res.status(404).json({
-				message: "The user id is not valid for this token.",
-			});
+        message: 'The user id is not valid for this token.',
+      });
     }
 
-    const newToken = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, {
-			algorithm: "HS256",
-			allowInsecureKeySizes: true,
-			expiresIn: process.env.JWT_EXPIRATION,
-		});
+    const newToken = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET,
+      {
+        algorithm: 'HS256',
+        allowInsecureKeySizes: true,
+        expiresIn: process.env.JWT_EXPIRATION,
+      },
+    );
 
     return res.status(200).json({
       message: 'Token refresh successful.',
@@ -133,14 +139,13 @@ exports.refreshToken = async (req, res) => {
           id: user.id,
           email: user.email,
           accessToken: newToken,
-          refreshToken: tokenExist.token
-        }
-      }
+          refreshToken: tokenExist.token,
+        },
+      },
     });
-
   } catch (error) {
     return res.status(500).json({
-      error: 'Internal server error.'
-    })
+      error: 'Internal server error.',
+    });
   }
-}
+};
